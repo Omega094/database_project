2858b9becb42725e56d5f939553fbb1f48d2b4e5
zjx <jzhao32@wustl.edu>
2016-06-03 21:43:54 -0500

    done

---
+++ CreateTable.py
@@ -0,0 +1,27 @@
+#This is the script to create the three tables 
+import psycopg2
+conn = psycopg2.connect("dbname=zhao887 user=zhao887")
+cur = conn.cursor()
+try:
+	cur.execute( 
+			"CREATE TABLE STOCK \
+			(\
+				NAME VARCHAR(50) NOT NULL PRIMARY KEY,\
+				COMPANY VARCHAR(50) NOT NULL\
+			);")
+	cur.execute( 
+			"CREATE TABLE STOCK_PRICE_DATA\
+			( \
+				DATA_ID INT NOT NULL PRIMARY KEY,\
+				NAME VARCHAR(50) NOT NULL REFERENCES STOCK(NAME),\
+				DATE INT NOT NULL,\
+				OPEN INT NOT NULL,\
+				HIGH INT NOT NULL,\
+				LOW INT NOT NULL,\
+				CLOSE INT NOT NULL,\
+				ADJ_CLOSE INT NOT NULL\
+			);")
+	conn.commit()
+except:
+	print "Failed to create the table."
+
