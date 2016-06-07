d5b22f0082f8a028196bca2d83bc2cd09d087922
zjx <zhao_j1@denison.edu>
2016-06-07 10:05:56 -0500

    done

--- queryData.py
+++ queryData.py
@@ -14,7 +14,7 @@ def extractStockVolumeAndInterestScore(stockName):
     for row in trendingResult:
         startDate = row[START_DATE]
         endDate = row[END_DATE]
-        interestScore = row[INTEREST_SCORE]
+        interestScore = row[INTEREST_SCORE][0][0]
         query = cur.execute("""SELECT sum(volume) FROM STOCK_PRICE_DATA WHERE date <= %s AND date >= %s AND name = %s""", (endDate, startDate, stockName, ) )
         weeklyVolume = cur.fetchall()
         combineResult.append( (stockName, startDate, endDate, weeklyVolume, interestScore) )
